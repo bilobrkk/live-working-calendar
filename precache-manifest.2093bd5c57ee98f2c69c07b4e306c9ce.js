c4e71d02a5f42aba17e661dc09f3d063dcbfb3c1
katjabilobrk <bilobrkk@gmail.com>
2020-07-16 11:21:26 +0200

    Updates

---
+++ precache-manifest.2093bd5c57ee98f2c69c07b4e306c9ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ae8bf2a7f92aa3885098e85ffecc9a",
+    "url": "/live-working-calendar/index.html"
+  },
+  {
+    "revision": "4d499c25cdf0c0206d94",
+    "url": "/live-working-calendar/static/css/main.82ea5b4d.chunk.css"
+  },
+  {
+    "revision": "2a0c306bc2e2c4628884",
+    "url": "/live-working-calendar/static/js/2.db9bbb9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/live-working-calendar/static/js/2.db9bbb9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d499c25cdf0c0206d94",
+    "url": "/live-working-calendar/static/js/main.a22f0bfb.chunk.js"
+  },
+  {
+    "revision": "98d788591e79a3c0060b",
+    "url": "/live-working-calendar/static/js/runtime-main.a476fffa.js"
+  }
+]);
